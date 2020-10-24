8148129018a191ed438113b0cd67c7f480e68205
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-10-24 16:31:18 +0100

    Update build.py

--- .github/workflows/build.py
+++ .github/workflows/build.py
@@ -1,3 +1,26 @@
+import os
+
+def get_filepaths(directory):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = [] # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+# Run the above function and store its results in a variable.   
+full_file_paths = get_filepaths("src")
+
 filename = "README.md"
 with open(filename, 'w') as file_object: # open file function with 'w' argument to allows write some text
     print('File has been opened successfully!\n') # if file successfully opened, this program will be shown
